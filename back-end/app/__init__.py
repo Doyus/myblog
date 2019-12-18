a5ef571ab54ff8975ca4197ef397c7c8367efd94
doyus <domingyus@163.com>
2019-12-18 17:12:48 +0800

    3. Flask设计User用户相关API

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,18 +1,26 @@
-from flask import Flask 
-from flask_cors import CORS 
-from config import Config 
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
+
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
+    # 启用 cors,负责前端会报错
     CORS(app)
-
+    db.init_app(app)
+    migrate.init_app(app,db)
     # 蓝图注册  blueprint
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
-    return app 
+    app.config['DEBUG'] = True
+    return app
 
 
-
+from app import models
--- back-end/config.py
+++ back-end/config.py
@@ -5,6 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_RUL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
     
